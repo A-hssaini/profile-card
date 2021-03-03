bc282ade5890b8e1636ad374bdd3da7630fcc568
Abdessamad Hssaini <ahssaini@e1r9p15.1337.ma>
2021-03-03 15:46:03 +0100

    prof-card

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ProfileCard from "./components/ProfileCard/ProfileCard";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ProfileCard
+        data={{
+          name: "HSSAINI ABDESSAMAD",
+          posts: "D4M45",
+          bio:
+            "Hello 👋, my name is Abdessamad. Software Engineer Student at ENSA   & 1337 KHOURIBGA, focusing on Web Dev technologies.",
+          location: "KHOURIBGA, MOROCCO",
+          technologies: [
+            "Javascript",
+            "React",
+            "PHP",
+            "HTML5",
+            "CSS3",
+            "SPRING",
+            "MySQL",
+            "DOCKER",
+            "C/C++",
+            "SHELL",
+            "JAVA",
+            "GIT"
+
+          ],
+          creationDate: "https://github.com/A-hssaini",
+        }}
+      />
+    </div>
+  );
+}
